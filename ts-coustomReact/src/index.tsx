f8186657d0290845bc09a3f9235719a3a6502689
luoye <luoye@start.com>
2020-09-20 01:16:22 +0800

    feat: ts-react增加自定义组件以及state，props

--- ts-coustomReact/src/index.tsx
+++ ts-coustomReact/src/index.tsx
@@ -1,13 +1,84 @@
-import { createElement as create, render } from './react/index'
+import { createElement as create, render, Component } from './react/index'
 
 const createElement = create;
 
+interface List {
+    key: number;
+    children?: List[]
+}
 
-const app = <div>
-    123
-<siv id="231">test</siv>
-    {1 + 3}
-    <div>test</div>
-</div>
+class Itme extends Component {
+    constructor() {
+        super();
+    }
+    render() {
+        return <div>{this.props.key}</div>
+    }
+}
 
-render(app, document.body);
+const insertState = (list: List[]) => {
+    return list.map(item => {
+        if (item.children) {
+            return insertState(item.children);
+        }
+        return (<Itme key={item.key} />)
+    })
+}
+
+
+
+class App extends Component {
+    constructor() {
+        super();
+    }
+    state = {
+        name: 'App',
+        list: [
+            {
+                key: 1,
+                children: [
+                    {
+                        key: 1,
+                        children: [
+                            {
+                                key: 1,
+                            },
+                            {
+                                key: 2,
+                            }
+                        ]
+                    },
+                    {
+                        key: 2,
+                    }
+                ]
+            },
+            {
+                key: 2,
+            }
+        ]
+    }
+
+
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.name}</h1>
+                {insertState(this.state.list)}
+                {this.props.children}
+                123
+                <div id="231">test</div>
+                <div>test</div>
+            </div>
+        )
+    }
+}
+
+
+
+render(<App name="app">
+    <div>
+        children
+    </div>
+</App>, document.body);
--- ts-coustomReact/src/react/index.ts
+++ ts-coustomReact/src/react/index.ts
@@ -1,19 +1,78 @@
-const createElement = (tagName: string, props: Record<string, string>, ...children: Array<string | HTMLElement>): HTMLElement => {
-    const root = document.createElement(tagName);
-    for (let p in props) {
-        root.setAttribute(p === 'className' ? 'class' : p, props[p]);
-    }
 
-    for (let child of children) {
-        if (typeof child !== 'object') {
-            let textNode = document.createTextNode(child);
-            root.appendChild(textNode);
-        } else {
-            root.appendChild(child);
+abstract class Component {
+    constructor() {
+        this.props = {
+            children: []
+        };
+        this._root = null;
+    }
+    props: Record<string, any>;
+    protected _root: HTMLElement;
+    get root() {
+        if (!this._root) {
+            this._root = this.render();
+        }
+        return this._root;
+    }
+    setAttribute(name: string, value: any) {
+        this.props[name] = value;
+    }
+    appendChild(node: HTMLElement | Text) {
+        this.props.children.push(node);
+    }
+    abstract render(): HTMLElement
+}
+
+class ElementWrapper {
+    constructor(type: string) {
+        this._root = document.createElement(type);
+    }
+    props: Record<string, any>;
+    protected _root: HTMLElement;
+    get root() {
+        return this._root;
+    }
+    setAttribute(name: string, value: string) {
+        this._root.setAttribute(name === 'className' ? 'class' : name, value)
+    }
+    appendChild(node: HTMLElement | Text) {
+        this._root.appendChild(node);
+    }
+}
+
+// 纯粹是为了得到一个继承于Component的类的类型，为createElement提供参数类型
+class ExtendsComponent extends Component {
+    render = (): HTMLElement => document.createElement('div');
+}
+
+const createElement = (
+    type: string | typeof ExtendsComponent,
+    props: Record<string, string> | null,
+    ...children: Array<string | HTMLElement | Array<string | HTMLElement>>
+): HTMLElement => {
+    let root: ElementWrapper | Component;
+    if (typeof type === 'string') {
+        root = new ElementWrapper(type);
+    } else {
+        root = new type();
+    }
+    for (let p in props) {
+        root.setAttribute(p, props[p]);
+    }
+    const insertChild = (children: Array<string | HTMLElement | Array<string | HTMLElement>>) => {
+        for (let child of children) {
+            if (typeof child !== 'object') {
+                let textNode = document.createTextNode(child);
+                root.appendChild(textNode);
+            } else if (child instanceof Array) {
+                insertChild(child)
+            } else {
+                root.appendChild(child);
+            }
         }
     }
-
-    return root;
+    insertChild(children);
+    return root.root;
 }
 
 const render = (element: HTMLElement, parentElement: HTMLElement) => {
@@ -21,6 +80,7 @@ const render = (element: HTMLElement, parentElement: HTMLElement) => {
 }
 
 export {
+    Component,
     createElement,
     render
 }
