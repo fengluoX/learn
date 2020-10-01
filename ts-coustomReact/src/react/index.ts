e1d335fc91ab0f1478892fddeb4367b7564a9c48
luoye <luoye@str.com>
2020-10-01 21:20:09 +0800

    feat: 完善ts-react

--- ts-coustomReact/src/react/index.ts
+++ ts-coustomReact/src/react/index.ts
@@ -1,26 +1,20 @@
-
 abstract class Component {
     constructor() {
         this.props = {
             children: []
         };
-        this._root = null;
+        this.range = null;
     }
     props: Record<string, any>;
     state?: Record<string, any>;
-    protected _root: HTMLElement;
-    private vdom:HTMLElement;
-    get root() {
-        if (!this._root) {
-            this._root = this.render();
-        }
-        this.vdom = this._root;
-        return this._root;
-    }
+    protected range: Range;
+    private oldVDom: Component;
     setAttribute(name: string, value: any) {
-        this.props[name] = value;
+        if (name !== 'children') {
+            this.props[name] = value;
+        }
     }
-    appendChild(node: HTMLElement | Text) {
+    appendChild(node: Component) {
         this.props.children.push(node);
     }
     setState(newState: Record<string, any>) {
@@ -38,48 +32,142 @@ abstract class Component {
             }
             merge(this.state, newState);
         }
-        this.rerender();
+        this.update();
     }
-    rerender() {
-        const { parentNode } = this._root;
-        parentNode.removeChild(this._root);
-        this._root = this.render();
-        parentNode.appendChild(this._root);
+    update(){
+        let isSameNode = (oldNode,newNode)=>{
+            if(oldNode.type!==newNode.type){
+                return false;
+            }
+            for(let name in newNode.props){
+                if(name==='children'){
+                    continue;
+                }
+                if(oldNode.props[name]!==newNode.props[name]){
+                    return false;
+                }
+            }
+            if(Object.keys(oldNode).length>Object.keys(newNode).length){
+                return false;
+            }
+            return true;
+        }
+        let update = (oldNode:Component,newNode:Component)=>{
+            if(!isSameNode(oldNode,newNode)){
+                newNode.renderToDom(oldNode.range);
+                return;
+            }
+            newNode.range = oldNode.range;
+            let newChildren = newNode.props.children;
+            let oldChildren = oldNode.props.children;
+
+            if(!newChildren||!newChildren.length){
+                return;
+            }
+            
+            let tailRange = oldChildren[oldChildren.length-1].range;
+            for(let i=0;i<newChildren.length;i++){
+                let newChild = newChildren[i];
+                let oldChild = oldChildren[i];
+                if(i<oldChildren.length){
+                    update(oldChild,newChild);
+                }else{
+                    let range = document.createRange();
+                    range.setStart(tailRange.endContainer,tailRange.endOffset);
+                    range.setEnd(tailRange.endContainer,tailRange.endOffset);
+                    newChild.renderToDom(range);
+                    tailRange = range;
+                }
+            }
+        }
+        let vdom = this.vdom;
+        update(this.oldVDom,vdom);
+        this.oldVDom = vdom;
     }
-    abstract render(): HTMLElement
+    get vdom() {
+        return this.render().vdom;
+    }
+    public renderToDom(range: Range) {
+        this.oldVDom = this.vdom;
+        this.range = range;
+        this.oldVDom.renderToDom(range);
+    }
+    abstract render(): Component
 }
 
-class ElementWrapper {
+function replaceRange(range: Range, node: HTMLElement | Text) {
+    range.insertNode(node);
+    range.setStartAfter(node);
+    range.deleteContents();
+    range.setStartBefore(node);
+    range.setEndAfter(node);
+}
+
+class ElementWrapper extends Component {
     constructor(type: string) {
-        this._root = document.createElement(type);
+        super();
+        this.type = type;
     }
-    props: Record<string, any>;
-    protected _root: HTMLElement;
-    get root() {
-        return this._root;
+    type: string;
+    get vdom() {
+        return this;
     }
-    setAttribute(name: string, value: any) {
-        if (name.match(/^on([\s\S]+)$/)) {
-            this._root.addEventListener(RegExp.$1.replace(/^[\s\S]/, c => c.toLocaleLowerCase()), value);
-        } else {
-            this._root.setAttribute(name === 'className' ? 'class' : name, value)
+    render() {
+        return new ElementWrapper(this.type);
+    }
+    public renderToDom(range: Range) {
+        const node = document.createElement(this.type);
+        for (let p in this.props) {
+            if (p !== 'children') {
+                if (p.startsWith('on')) {
+                    node.addEventListener(p.toLocaleLowerCase().slice(2), this.props[p]);
+                } else {
+                    node.setAttribute(p === 'className' ? 'class' : p, this.props[p]);
+                }
+            }
         }
+
+        this.props.children.forEach(item => {
+            const childRange = document.createRange();
+            childRange.setStart(node, node.childNodes.length);
+            childRange.setEnd(node, node.childNodes.length);
+            childRange.deleteContents();
+            item.renderToDom(childRange);
+        })
+        this.range = range;
+        replaceRange(range, node);
     }
-    appendChild(node: HTMLElement | Text) {
-        this._root.appendChild(node);
+}
+
+class TextWrapper extends Component {
+    constructor(content: string) {
+        super();
+        this.content = content;
+    }
+    content: string;
+    get vdom() {
+        return this;
+    }
+    render() {
+        return new TextWrapper(this.content);
+    }
+    public renderToDom(range: Range) {
+        this.range = range;
+        const node = document.createTextNode(this.content);
+        replaceRange(range, node);
     }
 }
 
 // 纯粹是为了得到一个继承于Component的类的类型，为createElement提供参数类型
 class ExtendsComponent extends Component {
-    render = (): HTMLElement => document.createElement('div');
+    render: () => Component;
 }
 
 const createElement = (
     type: string | typeof ExtendsComponent,
     props: Record<string, string> | null,
-    ...children: Array<string | HTMLElement | Array<string | HTMLElement>>
-): HTMLElement => {
+    ...children: Array<string | Component | Array<string | Component>>
+): Component => {
     let root: ElementWrapper | Component;
     if (typeof type === 'string') {
         root = new ElementWrapper(type);
@@ -89,13 +177,13 @@ const createElement = (
     for (let p in props) {
         root.setAttribute(p, props[p]);
     }
-    const insertChild = (children: Array<string | HTMLElement | Array<string | HTMLElement>>) => {
+    const insertChild = (children: Array<string | Component | Array<string | Component>>) => {
         for (let child of children) {
-            if(!child){
+            if (!child) {
                 continue;
             }
             if (typeof child !== 'object') {
-                let textNode = document.createTextNode(child);
+                let textNode = new TextWrapper(child);
                 root.appendChild(textNode);
             } else if (child instanceof Array) {
                 insertChild(child)
@@ -105,11 +193,15 @@ const createElement = (
         }
     }
     insertChild(children);
-    return root.root;
+    return root;
 }
 
-const render = (element: HTMLElement, parentElement: HTMLElement) => {
-    parentElement.appendChild(element);
+const render = (element: Component, parentElement: HTMLElement) => {
+    const range = document.createRange();
+    range.setStart(parentElement, 0);
+    range.setEnd(parentElement, parentElement.childNodes.length);
+    range.deleteContents();
+    element.renderToDom(range);
 }
 
 export {
--- ts-coustomReact/webpack.config.js
+++ ts-coustomReact/webpack.config.js
@@ -41,5 +41,6 @@ module.exports = {
     ],
     resolve:{
         extensions:['.js','.tsx','.ts']
-    }
+    },
+    devtool:'eval-source-map'
 }
